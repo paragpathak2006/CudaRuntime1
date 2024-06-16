e229c771c5eee50923d0787ffabeaf2d720d16eb
PARAG\parag <31978917+paragpathak2006@users.noreply.github.com>
2024-06-16 04:52:12 -0400

    update

--- kernel.cu
+++ kernel.cu
@@ -196,12 +196,12 @@ int main()
     int c[arraySize] = { 0 };
 
     // Add vectors in parallel.
-//    cudaError_t cudaStatus = addWithCuda(c, a, b, arraySize); _DEVICE_RESET_FAILED_
+    cudaError_t cudaStatus = addWithCuda(c, a, b, arraySize); _DEVICE_RESET_FAILED_
     printf("{1,2,3,4,5} + {10,20,30,40,50} = {%d,%d,%d,%d,%d}\n", c[0], c[1], c[2], c[3], c[4]);
 
     // cudaDeviceReset must be called before exiting in order for profiling and
     // tracing tools such as Nsight and Visual Profiler to show complete traces.
-//    cudaStatus = cudaDeviceReset(); _DEVICE_RESET_FAILED_
+    cudaStatus = cudaDeviceReset(); _DEVICE_RESET_FAILED_
 
     cout << endl << "**************************CUDA_TEST_SUCCESS********************************" << endl;
     cout << "**************************CUDA_TEST_SUCCESS********************************" << endl << endl;
